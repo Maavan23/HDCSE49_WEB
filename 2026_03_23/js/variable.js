5fb818df4132a4b80f09660dca95a57a8014dc48
Sasindu Kaveesha Gamapathirana <gsasindukaveesha@gmail.com>
2026-03-23 14:27:51 +0530

    new project today

---
+++ 2026_03_23/js/variable.js
@@ -0,0 +1,41 @@
+// let x=5;
+
+// console.log(x)
+
+// Declare
+// var name;
+
+//Initialization
+// name ="Sasindu"
+
+
+// console.log(name)
+
+/* ctrl + "/ " ----- making a comment shortcut */
+// shift + alt + A ----- making a comment in /* */ method
+// shift + arrow keys ------- select codes
+
+/* let name = 'Kaveesha'
+let age = 24;
+let firstName = undefined
+let selectColor = null;
+
+console.log("Check type", typeof(name));
+console.log("Checj type", typeof(age));
+console.log("Check type", typeof(firstName));
+console.log("Check type", typeof(selectColor)); */
+
+let score = 57;
+let grade;
+
+if(score >= 90){
+    grade = "A";    
+}else if(score >= 80){
+    grade = "B";
+}else if(score >= 70){
+    grade = "C";
+}else{
+    grade = "D or F";
+}
+
+console.log("Your grade id "+grade); 
