f608d68ac3bc104e819cb9529baac83f2cf9ce8f
Sasindu Kaveesha Gamapathirana <gsasindukaveesha@gmail.com>
2026-03-23 14:50:06 +0530

    update 4

---
+++ 2026_03_23/js/arrays.js
@@ -0,0 +1,8 @@
+let num = [10,20,30,40,50,60];
+
+console.log("First index Value", num[0]);
+console.log("Second index Value", num[1]);
+console.log("Third index Value", num[2]);
+console.log("Forth index Value", num[3]);
+console.log("Fifth index Value", num[4]);
+console.log("Sixth  index Value", num[5]);
