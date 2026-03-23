bf2253013ebc676f69d83bb49dbef66c1497811e
Sasindu Kaveesha Gamapathirana <gsasindukaveesha@gmail.com>
2026-03-23 14:36:27 +0530

    today 2nd update

---
+++ 2026_03_23/js/sample.js
@@ -0,0 +1,16 @@
+let day = 2;
+
+//one varible gainst many values --> switch method
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Monday");
+        break;
+    default:
+        console.log("Invalid Day")    
+}
