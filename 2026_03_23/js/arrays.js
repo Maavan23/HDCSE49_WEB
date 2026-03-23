85b55588a11ddfaa230b4aeaa2fc6c5550934669
Sasindu Kaveesha Gamapathirana <gsasindukaveesha@gmail.com>
2026-03-23 15:01:48 +0530

    update today 5

--- 2026_03_23/js/arrays.js
+++ 2026_03_23/js/arrays.js
@@ -5,4 +5,20 @@ console.log("Second index Value", num[1]);
 console.log("Third index Value", num[2]);
 console.log("Forth index Value", num[3]);
 console.log("Fifth index Value", num[4]);
-console.log("Sixth  index Value", num[5]);
+console.log("Sixth index Value", num[5]);
+
+// JavaScript for loop
+console.log("################# for Loop ####################")
+for(let i=0; i<num.length; i++){
+    console.log(num[i]);
+}
+
+// JavaScript while loop
+console.log("################# While Loop ####################")
+let i = 0;
+
+while(i<num.length){
+    console.log(num[i]);
+
+    i++;
+}
